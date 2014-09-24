a8366f2be869921b63e795c41ac4d6870f1c3a94
pepertyui <pepe_davila@live.com.mx>
2014-09-24 14:36:45 -0500

    mi programa

---
+++ holamundo/holamundo/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ALUMNOS
+ * Date: 24/09/2014
+ * Time: 02:34 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace holamundo
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			
+			Console.WriteLine("hola q hace");
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
